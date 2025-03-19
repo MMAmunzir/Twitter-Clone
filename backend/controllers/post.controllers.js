518fbc178d90d503fb1945b8de1860713d528791
MMAmunzir <munzirmuneer1998@gmail.com>
2025-03-19 15:26:33 +0530

    delete post function added and bug fixed

--- backend/controllers/post.controllers.js
+++ backend/controllers/post.controllers.js
@@ -47,7 +47,6 @@ export const deletePost = async (req, res) => {
     }
     await Post.findByIdAndDelete(req.params.id);
     res.status(200).json({ message: "post deleted successfully" });
-    res.json({ message: "post deleted successfully" });
   } catch (error) {
     res.status(500).json({ error: error.message });
     console.log(error);
