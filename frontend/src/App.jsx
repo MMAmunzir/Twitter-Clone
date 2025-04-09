ed52db1c830c9745f78f1f6d8371ee3ca04931e4
MMAmunzir <munzirmuneer1998@gmail.com>
2025-04-09 17:31:15 +0530

    Sidebar Typo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/auth/login/LoginPage";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
-import Sidebar from "./components/common/SideBar";
+import Sidebar from "./components/common/Sidebar";
 import NotificationPage from "./pages/notification/NotificationPage";
 import ProfilePage from "./pages/profile/ProfilePage";
 import RightPanel from "./components/common/RightPanel.jsx";
